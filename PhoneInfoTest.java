edd32a5fa784672595b1c1a0e5dfd8d99893f4eb
DongHoon <dhdnzk0304@gmail.com>
2015-10-08 02:12:37 +0900

    line

--- PhoneInfoTest.java
+++ PhoneInfoTest.java
@@ -5,9 +5,9 @@ package java_oop_practice;
  */
 public class PhoneInfoTest {
     public static void main (String[] args) {
-    PhoneInfo p1 = new PhoneInfo("홍길동", "010-123-1234", "2015.10.06");
-    PhoneInfo p2 = new PhoneInfo("가나다", "010-111-1111", "2000.01.01");
-    PhoneInfo p3 = new PhoneInfo("라마바", "010-987-6543");
+    PhoneInfo p1 = new PhoneInfo ( "홍길동", "010-123-1234", "2015.10.06" );
+    PhoneInfo p2 = new PhoneInfo ( "가나다", "010-111-1111", "2000.01.01" );
+    PhoneInfo p3 = new PhoneInfo ( "라마바", "010-987-6543" );
 
         p1.getInfo();
         p2.getInfo();

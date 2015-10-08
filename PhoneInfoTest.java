1952ffc8263d9f64576a2541ee6e4db2cee9dfb2
DongHoon <dhdnzk0304@gmail.com>
2015-10-08 15:00:08 +0900

    make select menu

--- PhoneInfoTest.java
+++ PhoneInfoTest.java
@@ -1,19 +1,35 @@
 package java_oop_practice;
 
+import java.util.Scanner;
 /**
  * Created by DH on 15. 10. 6..
  */
 public class PhoneInfoTest {
     public static void main (String[] args) {
-    PhoneInfo p1 = new PhoneInfo ( "홍길동", "010-123-1234", "2015.10.06" );
-    PhoneInfo p2 = new PhoneInfo ( "가나다", "010-111-1111", "2000.01.01" );
-    PhoneInfo p3 = new PhoneInfo ( "라마바", "010-987-6543" );
-    PhoneInfo p4 = new PhoneInfo ( "사아자", "010-345-4567" );
+        int choice;
+        Scanner keyboard = new Scanner( System.in );
 
-        p1.getInfo();
-        p2.getInfo();
-        p3.getInfo();
-        p4.getInfo();
+        while ( true ) {
+           ScreenOut.showMenu();
+           choice = keyboard.nextInt();
+
+           switch ( choice ) {
+               case 1:
+                    ScreenOut.askName();
+                    String name = keyboard.nextLine();
+                    ScreenOut.askPhoneNumber();
+                    String phoneNumber = keyboard.nextLine();
+                    ScreenOut.askBirthday();
+                    String birthday = keyboard.nextLine();
+
+                    PhoneInfo p1 = new PhoneInfo( name, phoneNumber, birthday );
+                    p1.getInfo();
+                   break;
+
+               case 2:
+                   System.out.println ( "terminate program." );
+                   return;
+           }
+        }
     }
 }
-

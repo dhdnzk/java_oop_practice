f298c068972cb7ca5993c8aa401f9fd118283536
DongHoon <dhdnzk0304@gmail.com>
2015-10-08 14:59:28 +0900

    modify

--- PhoneInfo.java
+++ PhoneInfo.java
@@ -5,40 +5,56 @@ package java_oop_practice;
  */
 public class PhoneInfo {
 
-    String name;
-    String phoneNumber;
-    String birthday;
+    private String name;
+    private String phoneNumber;
+    private String birthday;
 
     PhoneInfo ( String name, String phoneNumber, String birthday ) {
         this.name = name;
         this.phoneNumber = phoneNumber;
         this.birthday = birthday;
     }
-
     PhoneInfo ( String name, String phoneNumber ) {
         this.name = name;
         this.phoneNumber = phoneNumber;
         this.birthday = null;
     }
 
-    private String getName () {
+    String getName () {
         return this.name;
     }
-
-    private String getPhoneNumber () {
+    String getPhoneNumber () {
         return this.phoneNumber;
     }
-
-    private String getBirthday () {
+    String getBirthday () {
         return this.birthday;
     }
 
+    private void setName ( String name ) {
+        this.name = name;
+    }
+    private void setPhoneNumber ( String phoneNumber ) {
+        this.phoneNumber = phoneNumber;
+    }
+    private void setBirthday ( String birthday ) {
+       this.birthday = birthday;
+   }
+
+
     void getInfo() {
-        System.out.println( "이름 : " + this.getName() );
-        System.out.println( "전화번호 : " + this.getPhoneNumber() );
+        System.out.println( "name : " + this.getName() );
+        System.out.println( "phoneNumber : " + this.getPhoneNumber() );
         if ( this.getBirthday() != null ) {
-            System.out.println("생년월일 : " + this.getBirthday());
+            System.out.println("birthday : " + this.getBirthday());
         }
         System.out.println();
     }
+
+    void setInfo ( String name, String phoneNumber, String birthday ) {
+        setName(name);
+        setPhoneNumber( phoneNumber );
+        setBirthday( birthday );
+    }
+
 }
+

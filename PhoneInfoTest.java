c12c7de034db5c2dc58a7ad055f5415dd9ab5008
DongHoon <dhdnzk0304@gmail.com>
2015-10-12 20:16:43 +0900

    존재하지 않는 데이터 조회 및 삭제할 시에 NullPointerException 예외 처리(일단 메인함수에서 if else if 구문으로..)

--- PhoneInfoTest.java
+++ PhoneInfoTest.java
@@ -12,39 +12,55 @@ public class PhoneInfoTest {
         LinkedList l1 = new LinkedList(); // 리스트 추가
 
         while ( true ) {
-           ScreenOut.showMenu();
+            ScreenOut.showMenu();
 
-           switch ( keyboard.nextInt() ) {
-               case 1:
-                   ScreenOut.askName();
-                   String name = keyboard.next();
-                   ScreenOut.askPhoneNumber();
-                   String phoneNumber = keyboard.next();
-                   ScreenOut.askBirthday();
-                   String birthday = keyboard.next();
-                   l1.addData( name, phoneNumber, birthday );
-                   break;
+            switch ( keyboard.nextInt() ) {
+                case 1:
+                    ScreenOut.askName();
+                    String name = keyboard.next();
+                    ScreenOut.askPhoneNumber();
+                    String phoneNumber = keyboard.next();
+                    ScreenOut.askBirthday();
+                    String birthday = keyboard.next();
+                    l1.addData( name, phoneNumber, birthday );
+                    break;
 
-               case 2:
-                   System.out.print( "\n조회할 이름 입력 : " );
-                   ( l1.searchData( keyboard.next() ) ).getInfo();
-                   System.out.println( "입니다." );
-                   break;
+                case 2:
+                    System.out.print( "\n조회할 이름 입력 : " );
+                    PhoneInfo search = ( l1.searchData( keyboard.next() ) );
+                    if ( search != null ) {
+                        search.getInfo();
+                        System.out.println("입니다.");
+                    }
+                    else {
+                        System.out.println( "입력하신 데이터가 존재하지 않습니다." );
+                    }
+                    break;
 
-               case 3:
-                   System.out.print( "삭제할 이름 입력 : " );
-                   ( l1.delData( keyboard.next() ) ).getInfo();
-                    System.out.println( "삭제되었습니다." );
-                   break;
+                case 3:
+                    System.out.print( "삭제할 이름 입력 : " );
+                    PhoneInfo del = ( l1.delData(keyboard.next() ) );
+                    if ( del != null ) {
+                        del.getInfo();
+                        System.out.println( "삭제되었습니다." );
+                    }
+                    else {
+                        System.out.println ( "입력하신 데이터가 존재하지 않습니다." );
+                    }
+                    break;
 
-               case 4:
+                case 4:
                     l1.showAllData();
                     break;
 
-               case 5:
-                   System.out.println ( "terminate program." );
-                   return;
-           }
+                case 5:
+                    ScreenOut.screenClear();
+                    break;
+
+                case 6:
+                    System.out.println ( "terminate program." );
+                    return;
+            }
         }
     }
 }

0205f84d19cce4627e98e78d5342f1ae0ab1a92b
DongHoon <dhdnzk0304@gmail.com>
2015-10-12 14:44:38 +0900

    동적 할당 기반으로 전화번호부 작성하도록 클래스 추가(addData, searchData, delData메소드 아직 미구현)

---
+++ LinkedList.java
@@ -0,0 +1,48 @@
+package java_oop_practice;
+
+/**
+ * Created by DH on 15. 10. 12..
+ */
+public class LinkedList {
+    Node head;
+    Node tail;
+    Node cur;
+    Node before;
+    int numOfData;
+
+    LinkedList (  ) {
+        this.head = new Node();
+        this.tail = null;
+        this.cur = null;
+        this.before = null;
+        this.numOfData = 0;
+    }
+
+    void addData ( String name, String phoneNumber, String biorthday ) {
+        
+
+
+    }
+
+    PhoneInfo delData ( String name ) {
+
+
+
+    }
+
+    PhoneInfo searchData ( String name ) {
+
+
+
+    }
+}
+
+class Node {
+    PhoneInfo info;
+    Node next;
+
+    Node () {
+        this.info = null;
+        this.next = null;
+    }
+}

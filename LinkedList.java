a2ada54c43b3146e6f64147f588da3044dd00bdc
DongHoon <dhdnzk0304@gmail.com>
2015-10-12 18:13:10 +0900

    저장된 값 전체 출력 메소드 구현

--- LinkedList.java
+++ LinkedList.java
@@ -2,6 +2,7 @@ package java_oop_practice;
 
 /**
  * Created by DH on 15. 10. 12..
+ * Last modification 15. 10. 12..
  */
 public class LinkedList {
     Node head;
@@ -14,19 +15,36 @@ public class LinkedList {
         this.head = new Node();
         this.head.info = null;
         this.head.next = null;
-        this.tail = null;
+        this.tail = head;
         this.cur = null;
         this.before = null;
         this.numOfData = 0;
     }
 
-    void addData ( String name, String phoneNumber, String biorthday ) {
-
-
-
+    void addData ( String name, String phoneNumber, String birthday ) {
+        Node newNode = new Node( name, phoneNumber, birthday );
+        this.tail.next = newNode;
+        this.tail = newNode;
+        this.numOfData++;
     }
 
+    void showAllData () {
+
+        this.cur = this.head;
+        while( this.cur.next != null ) {
+            this.cur = this.cur.next;
+            this.cur.info.getInfo( );
+        }
+    }
+
+
+
+/*
     PhoneInfo delData ( String name ) {
+        if ( this.numOfData == 0 ) {
+            return null;
+        }
+
 
 
 
@@ -34,10 +52,18 @@ public class LinkedList {
 
     PhoneInfo searchData ( String name ) {
 
+    }
 
+    PhoneInfo LFirst () {
 
     }
+
+    PhoneInfo LNext () {
+
+    }
+    */
 }
+
 
 class Node {
     PhoneInfo info;
@@ -45,6 +71,11 @@ class Node {
 
     Node () {
         this.info = null;
+        this.next = null;
+    }
+
+    Node (String name, String phoneNumber, String birthday ) {
+        info = new PhoneInfo( name, phoneNumber, birthday);
         this.next = null;
     }
 }
